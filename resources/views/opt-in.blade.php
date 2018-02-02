c0494251087e526c6a1d325fbcaf0393977dc031
EffakT <dusaro123@gmail.com>
2018-02-02 17:37:32 +1300

    Adding Opt-In Instructions

--- resources/views/opt-in.blade.php
+++ resources/views/opt-in.blade.php
@@ -60,7 +60,15 @@
               <li>Halo Server App - SAPP</li>
             </ul>
             <h2>Set Up</h2>
-            <p>Coming Soon...</p>
+            <ul>
+              <li><a href="{{ URL::asset('js/hrl-files.zip') }}">Click here</a> to download the HRL files.</li>
+              <li>Copy the <strong>hrl_api.dll</strong> to your <strong>exe</strong> folder (where your <strong>haloded.exe</strong> file is).</li>
+              <li>Copy the <strong>lua folder</strong> to your <strong>exe</strong> folder.</li>
+              <li>Copy the <strong>hrl.lua</strong> to your <strong>lua</strong> folder (inside your sapp configuration).</li>
+              <li>If your Halo server is not running on port 2302, modify the <strong>hrl.lua</strong> and change the <strong>server_port</strong> variable to the port you are using.</li>
+              <li>You may need to update your init.txt file to contain <strong>lua 1</strong> if it does not already contain this.</li>
+              <li>Update your init.txt file to contain <strong>lua_load hrl</strong> after <strong>lua 1</strong>.</li>
+            </ul>
             <h2>Notes</h2>
             <p>Your server will not appear on this app instantly. Please give up to 15 minutes after first lap is recorded.</p>
             <p>If your server is still not displaying 15 minutes after the first lap is recorded, please <a href="#">contact me</a></p>

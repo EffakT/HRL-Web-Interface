46ebd0fa4fff8d988e61d5097d46800b2c93bf55
Damian <dusaro123@gmail.com>
2020-04-03 14:51:43 +1300

    Adding check for user before checking if server is claimed

--- resources/views/leaderboard/server.blade.php
+++ resources/views/leaderboard/server.blade.php
@@ -3,13 +3,14 @@
 @section('pageTitle', 'Leaderboard for '.$server->name)
 
 @section('content')
+    @php $user = \Illuminate\Support\Facades\Auth::user(); @endphp
     <div class="container">
 
         <div class="row">
             <div class="col-lg-12">
                 @if (!$server->isClaimed())
                     <a href="{{route('server:manage', $server)}}" class="btn btn-primary mb-2">Claim Server</a>
-                @elseif ($server->isClaimedBy(\Illuminate\Support\Facades\Auth::user()))
+                @elseif (!is_null($user) && $server->isClaimedBy($user))
                     <a href="{{route('server:manage', $server)}}" class="btn btn-primary mb-2">Manage Server</a>
                 @endif
 

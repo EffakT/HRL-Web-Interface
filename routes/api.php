72f085e17cb8a584237e3cabbcdaaef84aa2bed8
Damian <dusaro123@gmail.com>
2020-04-12 10:38:52 +1200

    Disable user api endpoint as not needed

--- routes/api.php
+++ routes/api.php
@@ -13,7 +13,7 @@ use Illuminate\Http\Request;
 |
 */
 
-Route::middleware('auth:api')->get('/user', 'ApiController@user');
+//Route::middleware('auth:api')->get('/user', 'ApiController@user');
 
 Route::get('/newtime', 'ApiController@newTime');
 Route::post('/newtime', 'ApiController@newTime');

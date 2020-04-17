012cd7a017b1a2494f4fa3e835170c6d727d85c0
Damian <dusaro123@gmail.com>
2020-04-17 12:22:37 +1200

    Add discord to contact page

--- resources/views/contact.blade.php
+++ resources/views/contact.blade.php
@@ -13,7 +13,8 @@
                 <h2>Ways to contact me</h2>
                 <p>You can contact me by filling out the form on my <a href="http://www.effakt.info/contact"
                                                                        target="_blank">portfolio</a>.
-                    <br>Or you can contact me on steam <a href="http://steamcommunity.com/id/effakt" target="_blank">EffakT</a>.
+                    <br>Or you can contact me on steam <a href="http://steamcommunity.com/id/effakt" target="_blank">EffakT</a>
+                    <br>Or via discord <b>effakt#4666</b>.
                 </p>
             </div>
         </div>

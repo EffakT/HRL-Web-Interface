689695c637f6ea22019100017af328b3ddf907c3
Damian <dusaro123@gmail.com>
2020-05-18 18:11:08 +1200

    Remove Known Aliases for security

--- resources/views/leaderboard/player.blade.php
+++ resources/views/leaderboard/player.blade.php
@@ -8,17 +8,6 @@
         <div class="row">
             <div class="col-lg-12">
                 <h1>Leaderboard for {{$player->name}}</h1>
-    
-                <strong>Known Aliases:</strong>
-                <ul class="list-inline list-unstyled">
-                    @foreach ($player->alias() AS $alias)
-                        <li class="list-inline-item">
-                            <a href="{{route('player', $alias->id)}}">
-                                {{$alias->name}}
-                            </a>
-                        </li>
-                    @endforeach
-                </ul>
 
                 <singleplayer ajax="{{ route('player', $player->id)  }}"></singleplayer>
             </div>
